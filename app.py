301b01145e5aa0777089d6665293dcc3e707d02a
starnold <starnold@redhat.com>
2021-07-06 13:35:14 +0100

    my new change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World from the pipeline demo"
+    return "Hello World from the pipeline with my new change"
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
