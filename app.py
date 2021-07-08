630baad488a3bb3d195658a7cf0fe827aa7ab411
starnold <starnold@redhat.com>
2021-07-08 10:25:13 +0100

    football message

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World"
+    return "Hello World - maybe football is coming home"
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
