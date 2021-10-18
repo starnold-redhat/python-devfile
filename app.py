69d3dc50a3367f8cbe237d8957fb4768576f77df
starnold-redhat <86602023+starnold-redhat@users.noreply.github.com>
2021-10-18 13:06:17 +0100

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello World !!!"
+    return "Hello Summit !!!"
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
