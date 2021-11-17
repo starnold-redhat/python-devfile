275c7a766124aabec3c65c1c9a3a624569b3002f
starnold-redhat <86602023+starnold-redhat@users.noreply.github.com>
2021-11-17 11:45:55 +0000

    wednesday change

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Hello Summit London on Tuesday - Pizza time!!!"
+    return "Hello Wednesday - Pizza time!!!"
 
 if __name__ == '__main__':
     app.run(port=8080,host='0.0.0.0')
